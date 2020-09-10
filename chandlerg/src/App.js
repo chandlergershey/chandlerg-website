5fbccf77b956eddfa4a0198e93a2277f382cb9fc
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-09-10 13:34:10 -0400

    Navigation links working with react router

--- chandlerg/src/App.js
+++ chandlerg/src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
+
+import Home from './home/Home';
+import Design from './design/Design';
+import Music from './music/Music';
+import Store from './store/Store';
+import Blog from './blog/Blog';
+import Videos from './videos/Videos';
+import AboutMe from './aboutme/AboutMe';
+
 import './App.css';
+
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="">
+      <Router>
         <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/design" exact component={Design}/>
+          <Route path="/music" exact component={Music}/>
+          <Route path="/store" exact component={Store}/>
+          <Route path="/blog" exact component={Blog}/>
+          <Route path="/videos" exact component={Videos}/>
+          <Route path="/aboutme" exact component={AboutMe}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- chandlerg/src/components/navbar/Navbar.js
+++ chandlerg/src/components/navbar/Navbar.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import './Navbar.css';
+import {Link} from 'react-router-dom';
+
+// component imports
+import Home from '../../home/Home';
+import Design from '../../design/Design';
+import Music from '../../music/Music';
+import Store from '../../store/Store';
+import Blog from '../../blog/Blog';
+import Videos from '../../videos/Videos';
+import AboutMe from '../../aboutme/AboutMe';
 
 // Text imports
 import ChandlerGText from '../../assets/text_images/text_chandler_g.svg';
@@ -15,16 +25,16 @@ function Navbar() {
     <div className="Nav-component">
 
       <div className="Nav-logo">
-        <img src={ChandlerGText} className="Nav-text" alt="logo" />
+        <Link to='/'><img src={ChandlerGText} className="Nav-text" alt="Chandler G website" /></Link>
       </div>
 
       <div className="Nav-objects">
-        <img src={MusicText} className="Nav-text" alt="logo" />
-        <img src={DesignText} className="Nav-text" alt="logo" />
-        <img src={StoreText} className="Nav-text" alt="logo" />
-        <img src={BlogText} className="Nav-text" alt="logo" />
-        <img src={VideosText} className="Nav-text" alt="logo" />
-        <img src={AboutMeText} className="Nav-text" alt="logo" />
+        <Link to='/music'><img src={MusicText} className="Nav-text" alt="Music tab" /></Link>
+        <Link to='/design'><img src={DesignText} className="Nav-text" alt="Design tab" /></Link>
+        <Link to='/store'><img src={StoreText} className="Nav-text" alt="Store tab" /></Link>
+        <Link to='/blog'><img src={BlogText} className="Nav-text" alt="Blog tab" /></Link>
+        <Link to='/videos'><img src={VideosText} className="Nav-text" alt="Videos tab" /></Link>
+        <Link to='/aboutme'><img src={AboutMeText} className="Nav-text" alt="About me tab" /></Link>
       </div>
 
     </div>
