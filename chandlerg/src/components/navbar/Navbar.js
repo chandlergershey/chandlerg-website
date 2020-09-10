e2ee45a973adf28d572fe1df5419f2b0a2250f8f
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-09-10 12:15:47 -0400

    Got the navbar in place

--- chandlerg/src/components/navbar/Navbar.js
+++ chandlerg/src/components/navbar/Navbar.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './Navbar.css';
 
+// Text imports
+import ChandlerGText from '../../assets/text_images/text_chandler_g.svg';
+import MusicText from '../../assets/text_images/text_music.svg';
+import DesignText from '../../assets/text_images/text_design.svg';
+import StoreText from '../../assets/text_images/text_store.svg';
+import BlogText from '../../assets/text_images/text_blog.svg';
+import VideosText from '../../assets/text_images/text_videos.svg';
+import AboutMeText from '../../assets/text_images/text_about_me.svg';
+
 function Navbar() {
   return (
-    <div className="">
-      <p>Navbar!</p>
+    <div className="Nav-component">
+
+      <div className="Nav-logo">
+        <img src={ChandlerGText} className="Nav-text" alt="logo" />
+      </div>
+
+      <div className="Nav-objects">
+        <img src={MusicText} className="Nav-text" alt="logo" />
+        <img src={DesignText} className="Nav-text" alt="logo" />
+        <img src={StoreText} className="Nav-text" alt="logo" />
+        <img src={BlogText} className="Nav-text" alt="logo" />
+        <img src={VideosText} className="Nav-text" alt="logo" />
+        <img src={AboutMeText} className="Nav-text" alt="logo" />
+      </div>
+
     </div>
   );
 }
