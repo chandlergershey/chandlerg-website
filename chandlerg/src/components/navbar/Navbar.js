2fbd18a10b6ba8e71aad84d48d35b5ab1f60cb9c
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-09-15 14:29:01 -0400

    Progress making the navbar more user friendly

--- chandlerg/src/components/navbar/Navbar.js
+++ chandlerg/src/components/navbar/Navbar.js
@@ -19,25 +19,40 @@ import StoreText from '../../assets/text_images/text_store.svg';
 import BlogText from '../../assets/text_images/text_blog.svg';
 import VideosText from '../../assets/text_images/text_videos.svg';
 import AboutMeText from '../../assets/text_images/text_about_me.svg';
+import DropdownIcon from '../../assets/icons/dropdown.png';
 
 function Navbar() {
   return (
-    <div className="Nav-component">
+    <div>
+      <div className="Nav-component">
 
-      <div className="Nav-logo">
-        <Link to='/'><img src={ChandlerGText} className="Nav-text" alt="Chandler G website" /></Link>
+        <div className="Nav-logo">
+          <Link to='/'><img src={ChandlerGText} className="Nav-text" alt="Chandler G website" /></Link>
+        </div>
+
+        <div className="Nav-objects">
+          <Link to='/music'><img src={MusicText} className="Nav-text" alt="Music tab" /></Link>
+          <Link to='/design'><img src={DesignText} className="Nav-text" alt="Design tab" /></Link>
+          <Link to='/store'><img src={StoreText} className="Nav-text" alt="Store tab" /></Link>
+          <Link to='/blog'><img src={BlogText} className="Nav-text" alt="Blog tab" /></Link>
+          <Link to='/videos'><img src={VideosText} className="Nav-text" alt="Videos tab" /></Link>
+          <Link to='/aboutme'><img src={AboutMeText} className="Nav-text" alt="About me tab" /></Link>
+        </div>
+
+        <img src={DropdownIcon} className="Nav-dropdown-button" alt="drop down"></img>
       </div>
-
-      <div className="Nav-objects">
-        <Link to='/music'><img src={MusicText} className="Nav-text" alt="Music tab" /></Link>
-        <Link to='/design'><img src={DesignText} className="Nav-text" alt="Design tab" /></Link>
-        <Link to='/store'><img src={StoreText} className="Nav-text" alt="Store tab" /></Link>
-        <Link to='/blog'><img src={BlogText} className="Nav-text" alt="Blog tab" /></Link>
-        <Link to='/videos'><img src={VideosText} className="Nav-text" alt="Videos tab" /></Link>
-        <Link to='/aboutme'><img src={AboutMeText} className="Nav-text" alt="About me tab" /></Link>
+      <div className="Nav-dropdown">
+        <div className="Nav-dropdown-object-container">
+          <Link to='/music'><img src={MusicText} className="Nav-dropdown-text" alt="Music tab" /></Link>
+          <Link to='/design'><img src={DesignText} className="Nav-dropdown-text" alt="Design tab" /></Link>
+          <Link to='/store'><img src={StoreText} className="Nav-dropdown-text" alt="Store tab" /></Link>
+          <Link to='/blog'><img src={BlogText} className="Nav-dropdown-text" alt="Blog tab" /></Link>
+          <Link to='/videos'><img src={VideosText} className="Nav-dropdown-text" alt="Videos tab" /></Link>
+          <Link to='/aboutme'><img src={AboutMeText} className="Nav-dropdown-text" alt="About me tab" /></Link>
+        </div>
       </div>
-
     </div>
+    
   );
 }
 
