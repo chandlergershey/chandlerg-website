04a3740a35af965c1affed68a71cd8e39ece6c8e
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-09-15 16:25:01 -0400

    Navbar formats nearly perfectly on mobile devices

--- chandlerg/src/components/navbar/Navbar.js
+++ chandlerg/src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Navbar.css';
 import {Link} from 'react-router-dom';
+import $ from 'jquery';
 
 // component imports
 import Home from '../../home/Home';
@@ -19,11 +20,32 @@ import StoreText from '../../assets/text_images/text_store.svg';
 import BlogText from '../../assets/text_images/text_blog.svg';
 import VideosText from '../../assets/text_images/text_videos.svg';
 import AboutMeText from '../../assets/text_images/text_about_me.svg';
+import HomeText from '../../assets/text_images/text_home.svg';
 import DropdownIcon from '../../assets/icons/dropdown.png';
 
 function Navbar() {
+
+  var navBarDisplayed = true;
+
+  function navBarButtonClicked() {
+    if(navBarDisplayed){
+      document.getElementById("nav").style.display = "none";
+      navBarDisplayed = false;
+    } else {
+      document.getElementById("nav").style.display = "block";
+      navBarDisplayed = true;
+    }
+  }
+
+  function navBarOptionsClicked(){
+    document.getElementById("nav").style.display = "none";
+    navBarDisplayed = false;
+  }
+
   return (
+    
     <div>
+      
       <div className="Nav-component">
 
         <div className="Nav-logo">
@@ -39,16 +61,17 @@ function Navbar() {
           <Link to='/aboutme'><img src={AboutMeText} className="Nav-text" alt="About me tab" /></Link>
         </div>
 
-        <img src={DropdownIcon} className="Nav-dropdown-button" alt="drop down"></img>
+        <img onClick={navBarButtonClicked} src={DropdownIcon} className="Nav-dropdown-button" alt="drop down"></img>
       </div>
-      <div className="Nav-dropdown">
+      <div id="nav" className="Nav-dropdown">
         <div className="Nav-dropdown-object-container">
-          <Link to='/music'><img src={MusicText} className="Nav-dropdown-text" alt="Music tab" /></Link>
-          <Link to='/design'><img src={DesignText} className="Nav-dropdown-text" alt="Design tab" /></Link>
-          <Link to='/store'><img src={StoreText} className="Nav-dropdown-text" alt="Store tab" /></Link>
-          <Link to='/blog'><img src={BlogText} className="Nav-dropdown-text" alt="Blog tab" /></Link>
-          <Link to='/videos'><img src={VideosText} className="Nav-dropdown-text" alt="Videos tab" /></Link>
-          <Link to='/aboutme'><img src={AboutMeText} className="Nav-dropdown-text" alt="About me tab" /></Link>
+          <Link to='/'><img src={HomeText} className="Nav-dropdown-text" onClick={navBarOptionsClicked} alt="Home tab" /></Link>
+          <Link to='/music'><img src={MusicText} className="Nav-dropdown-text" onClick={navBarOptionsClicked} alt="Music tab" /></Link>
+          <Link to='/design'><img src={DesignText} className="Nav-dropdown-text" onClick={navBarOptionsClicked} alt="Design tab" /></Link>
+          <Link to='/store'><img src={StoreText} className="Nav-dropdown-text" onClick={navBarOptionsClicked} alt="Store tab" /></Link>
+          <Link to='/blog'><img src={BlogText} className="Nav-dropdown-text" onClick={navBarOptionsClicked} alt="Blog tab" /></Link>
+          <Link to='/videos'><img src={VideosText} className="Nav-dropdown-text" onClick={navBarOptionsClicked} alt="Videos tab" /></Link>
+          <Link to='/aboutme'><img src={AboutMeText} className="Nav-dropdown-text" onClick={navBarOptionsClicked} alt="About me tab" /></Link>
         </div>
       </div>
     </div>
