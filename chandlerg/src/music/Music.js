3a6e3302c5de652cbd66efb30bbc31cebb1caf9f
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-09-22 19:11:39 -0400

    Music feature

--- chandlerg/src/music/Music.js
+++ chandlerg/src/music/Music.js
@@ -7,7 +7,12 @@ function Music() {
   return (
     <div className="">
       <p className="Hello-text">Chandler G Music!</p>
-      <img src={AwakenCover} className="Awaken-cover" alt="Awaken cover" />
+      {/* <img src={AwakenCover} className="Awaken-cover" alt="Awaken cover" /> */}
+
+      <div>
+        <iframe width="100%" height={300} scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/884358778&color=%23e4c0d8&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" /><div style={{fontSize: '10px', color: '#cccccc', lineBreak: 'anywhere', wordBreak: 'normal', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight: 100}}><a href="https://soundcloud.com/chandler_g" title="Chandler G" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>Chandler G</a> · <a href="https://soundcloud.com/chandler_g/awaken" title="Awaken" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>Awaken</a></div>
+      </div>
+
       <PageUnderConstruction />
     </div>
   );
