96a9948b0486382b6c737b3218869ec18ee3a97e
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-09-22 17:44:04 -0400

    Saving changes

--- chandlerg/src/design/Design.js
+++ chandlerg/src/design/Design.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Design.css';
 import PageUnderConstruction from '../components/PageUnderConstruction';
 import SteveJobsImage from '../assets/portfolio/steve/steve.jpg';
+import Cards from '../components/Cards';
 
 function Design() {
   return (
     <div className="">
       <p className="Hello-text">My Design!</p>
+      <Cards />
       <img src={SteveJobsImage} className="Steve-jobs-drawing" alt="Steve Jobs drawing" />
       <PageUnderConstruction />
       
--- chandlerg/src/store/Store.js
+++ chandlerg/src/store/Store.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Store.css';
 import PageUnderConstruction from '../components/PageUnderConstruction';
+import { Button } from '../components/Button';
 
 function Store() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  }
+
+  window.addEventListener('resize', showButton);
+
+
   return (
     <div className="">
       <p className="Hello-text">The Store!</p>
-      <PageUnderConstruction />
+
+      <div className='menu-icon' onClick={handleClick}>
+        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+      </div>
+      
+      {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+
+      {/* <PageUnderConstruction /> */}
     </div>
   );
 }
