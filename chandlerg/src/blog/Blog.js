22ab79b7d242abfb4276440ec06f23b8403a5b19
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-09-23 18:45:52 -0400

    Built blog page, videos page, fixed portfolio

--- chandlerg/src/blog/Blog.js
+++ chandlerg/src/blog/Blog.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Blog.css';
 import PageUnderConstruction from '../components/PageUnderConstruction';
+import BlogCards from '../components/BlogCards';
 
 function Blog() {
   return (
     <div className="">
       <p className="Hello-text">My Blog!</p>
-      <PageUnderConstruction />
+      <BlogCards />
+      {/* <PageUnderConstruction /> */}
     </div>
   );
 }
--- chandlerg/src/videos/Videos.js
+++ chandlerg/src/videos/Videos.js
@@ -9,9 +9,28 @@ function Videos() {
     <div className="">
       <p className="Hello-text">My Videos!</p>
       <div className="YouTube-player-container">
-        <YouTube videoId="dth148PBXl4"  />
+        {/* <YouTube videoId="dth148PBXl4"  /> */}
+        <div
+          className="video"
+          style={{
+            position: "relative",
+            paddingBottom: "100%" /* 16:9 */,
+            paddingTop: 25,
+            height: 0
+          }}>
+          <iframe className="iframe-div"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0
+            }}
+            src='https://www.youtube.com/embed/dth148PBXl4'
+            frameBorder="0"
+          />
+        </div>
       </div>
-      <PageUnderConstruction />
+      x
+      {/* <PageUnderConstruction /> */}
     </div>
   );
 }
