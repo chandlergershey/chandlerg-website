bb4b4989a853f89ff2fe7d7d329d423cf94a9054
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-09-25 17:19:29 -0400

    Test

--- chandlerg/src/index.js
+++ chandlerg/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+// Amplify.configure(awsExports);
 
 ReactDOM.render(<App />, document.getElementById('root'));
