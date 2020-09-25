0ca2f43a30425c6526deabd66c05ffb16ba8b38d
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-09-25 17:35:26 -0400

    I think this will work again

--- chandlerg/src/index.js
+++ chandlerg/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Amplify from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+// import Amplify from 'aws-amplify';
+// import awsExports from './aws-exports';
+// Amplify.configure(awsExports);
 
 ReactDOM.render(<App />, document.getElementById('root'));
