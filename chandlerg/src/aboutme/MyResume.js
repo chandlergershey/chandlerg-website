97f4f7e06090516d448c0118f2ec293c43d457a0
Chandler <chandlergershey@example.com>
2021-07-26 10:36:37 -0400

    Reconnect repository

--- chandlerg/src/aboutme/MyResume.js
+++ chandlerg/src/aboutme/MyResume.js
@@ -15,21 +15,34 @@ function MyResume() {
           </div>
           <div className="Resume-header-objective">
           <b>Objective</b> 
-          <p>To use my computer science expertise and exceptional graphic design skills to obtain an entry-level software developer position in a company with great culture, people, and products.</p>
+          <p>To use my computer science knowledge and graphic design skills to obtain a mid-level web developer position at a team-driven company focused on developing exceptional products.</p>
           </div>
         </div>
+        
 
         <div className="Resume-work-experience">
           <h1>Work Experience</h1>
           <div className="Resume-job">
             <div className="Resume-job-header">
+              <b>Bear Cognition - Full Stack Web Developer</b> | Charleston, SC | Nov 2020 - Present
+            </div>
+            <div className="Resume-job-description">
+              <p>•  Lead web developer on an athlete management web application using React frontend and AWS Cloud Services building the project with TypeScript, Material UI, HTML, SASS, REST API, AWS S3, AWS CloudFront, and TerraForm. </p>
+              <p>• Architected data model patterns, designed requirements from customer inquiries, created mock prototypes using Figma, and used Adobe Illustrator to design assets for the site</p>
+              <p>• Connecting to backend APIs to receive and send JSON data to and from AWS Dynamo DB</p>
+              <p>• Managing code version control in GitHub and Agile sprints in Jira</p>
+            </div>
+          </div>
+
+          <div className="Resume-job">
+            <div className="Resume-job-header">
               <b>Nationwide Children’s Hospital - iOS Mobile Software Developer</b> | Columbus, OH | Jan 2020 - April 2020
             </div>
             <div className="Resume-job-description">
-              <p>• Project manager of Agile driven team which developed a mood and voice tracking app for therapy patients</p>
-              <p>• Programmed in SwiftUI creating the app’s navigation bar, settings, mood selection, audio recording functionality, login and signup, and trends page</p>
-              <p>• Connected app backend to AWS Cloud Services: Cognito, Amplify, AppSync, DynamoDB, S3</p>
-              <p>• Managed project tasks in Jira, code version control in GitHub, and conducted weekly meetings with stakeholders</p>
+              <p>•  Project manager of Agile driven team developing an iOS application to track the mood of therapy patients</p>
+              <p>•  Programmed in SwiftUI to create the app’s navigation bar, mood selection page, settings, audio recording functionality, login and signup authentication, and analytics page</p>
+              <p>• Connected app backend to AWS Cloud Services: Amplify, Cognito, AppSync, DynamoDB, S3</p>
+              <p>• Conducted weekly meetings with stakeholders and other team members</p>
             </div>
           </div>
 
@@ -42,7 +55,6 @@ function MyResume() {
               <p>• Redesigned the user interface of JadeTrack’s site with HTML, CSS, JavaScript, and jQuery to allow navigation bar and data tables to format to mobile views</p>
               <p>• Built new audit reporting functionality to give customers the option to resolve, edit, and delete audit issues</p>
               <p>• Built a sign-up portal for a customer, an informational site on energy usage for a client school district, and custom widgets such as a temperature controller, energy converter, and Air Quality Index with user-inputted zip code</p>
-              <p>• Redesigned C# and SQL models to allow the assignment of JadeTrack program managers to our clients</p>
             </div>
           </div>
 
@@ -90,15 +102,11 @@ function MyResume() {
         <div className="Resume-projects">
           <h1 className="Resume-projects-header">Projects</h1>
           <div className="Resume-projects-description">
-          • <b>Carpool Recycling Android App:</b> Tracks an individual’s recycling metrics, send message other users, and create recycling groups with friends. Built with Java, Kotlin, and a Firebase backend
+          • <b>SpaceX Website:</b> : Built a SpaceX website that accesses SpaceX API data and displays the information. A beautiful display of my UI/UX design skills (spacex.chandlerg.com, username: testtest, password: testtest)
           </div>
           <div className="Resume-projects-description">
-          • <b>Swaypoints Website:</b> A Slack application for team voting on issues within Slack; I built the website and designed graphics (www.swaypoints.com)
+          • <b>The Gauntlet Series:</b> My friends and I founded and hosted a 3v3 soccer tournament. I built the website (www.thegauntletseries.com), designed merchandise, advertisements, logos, and video
           </div>
-          <div className="Resume-projects-description">
-          • <b>The Gauntlet Series:</b> My friends and I founded and hosted a 3v3 soccer tournament. I built the website (www.thegauntletseries.com), designed merchandise, advertisements, logos, and videos
-          </div>
-
         </div>
 
         <div className="Resume-links">
